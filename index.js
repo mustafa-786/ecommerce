64bd2aa68056101a7ad501c724872167b5ba20ff
mustafa <mustafa.shikarpur@zymr.com>
2022-06-14 10:00:16 +0530

    removed console.log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var discount_code1 =document.getElementById('discount_code1')
 var discount_code_msg=document.getElementById('discount_code_msg')
 var product_total_amt=document.getElementById('product_total_amt')
 var total_cart_amt = document.getElementById('total_cart_amt')
+var shipping_charge = document.getElementById('shipping_charge')
 function discount()
 {
   if(discount_code1.value ==='mustafa')
@@ -13,8 +14,6 @@ function discount()
     else{
     total_cart_amt.innerHTML =parseInt(total_cart_amt.innerHTML) -15 
     }
-    
-    //current_total_amount -15
   }
   else{
     discount_code_msg.innerText='Try Again! Valid code is mustafa'
@@ -24,10 +23,7 @@ function discount()
 function increment(incdec,itemval) {
   var textbox = document.getElementById(incdec);
   var itemval = document.getElementById(itemval);
- 
-  var shipping_charge = document.getElementById('shipping_charge')
-  console.log(total_cart_amt)
-  console.log(product_total_amt)
+
   if(textbox.value>=5)
   {
     
@@ -39,8 +35,7 @@ function increment(incdec,itemval) {
   else{
     textbox.value = parseInt(textbox.value) +1
     itemval.innerHTML= parseInt(itemval.innerHTML) +15
-    product_total_amt.innerHTML = parseInt( product_total_amt.innerHTML) + 15
-    
+    product_total_amt.innerHTML = parseInt( product_total_amt.innerHTML) + 15   
     total_cart_amt.innerHTML= parseInt(product_total_amt.innerHTML) + parseInt(shipping_charge.innerHTML)
   }
 }
@@ -49,7 +44,6 @@ function decrement(incdec,itemval) {
   var textbox = document.getElementById(incdec);
   var itemval = document.getElementById(itemval);
  
-  var shipping_charge = document.getElementById('shipping_charge')
   if(textbox.value <=0)
   {
     textbox.value=0
